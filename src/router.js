cedfbcc853ee7b9d4c9c8a222a33b69ef1d074b8
John Moore <john.moore@snh.gov.uk>
2020-09-04 13:20:21 +0100

    Add route to router
    Issue: Scottish-Natural-Heritage/Licensing#391

--- src/router.js
+++ src/router.js
@@ -4,13 +4,23 @@ const router = express.Router();
 // Import all the controllers.
 import {Page} from './controllers/_base.js';
 import StartController from './controllers/start.js';
+import InformationController from './controllers/information.js';
 
 // Configure all of the pages and routes.
 
 router.use(
   Page({
     path: 'start',
+    positiveForward: 'information',
     controller: StartController
+  })
+);
+
+router.use(
+  Page({
+    path: 'information',
+    back: 'start',
+    controller: InformationController
   })
 );
 
