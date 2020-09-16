90a9eeab1192f37edd9694762c7851f1c1b54c70
John Moore <john.moore@snh.gov.uk>
2020-09-16 10:19:23 +0100

    Add further integration tests
    Issue: Scottish-Natural-Heritage/Licensing#344

--- cypress/integration/006-firearm.js
+++ cypress/integration/006-firearm.js
@@ -30,7 +30,7 @@ describe('Firearm page ', function () {
 
   it('should allow access if the user visits all the pages in order', function () {
     cy.visit('/firearm');
-    cy.get('h1').should('contain', 'Do you have a Firearm Certificate?');
+    cy.get('h1').should('contain', 'Firearm Certificate details');
   });
 
   it('blank entries + main button should navigate to same page with error', function () {
@@ -42,6 +42,48 @@ describe('Firearm page ', function () {
 
     cy.get('.govuk-error-summary ul li a')
       .should('contain', 'You must enter the number found on your firearm certificate.')
-      .and('contain', 'You must enter the date you aquired your firearm certificate.');
+      .and('contain', 'Please enter a valid year')
+      .and('contain', 'Please enter a valid month')
+      .and('contain', 'Please enter a valid day');
+  });
+
+  it('filled-out entries with future date + main button should navigate to same page with error', function () {
+    cy.visit('/firearm');
+
+    cy.get('input[type="text"]#certificate-number').type('123456789');
+    cy.get('input[type="text"]#certificate-issued-certificateIssuedDay').type('01');
+    cy.get('input[type="text"]#certificate-issued-certificateIssuedMonth').type('01');
+    cy.get('input[type="text"]#certificate-issued-certificateIssuedYear').type('2999');
+
+    cy.get('#main-content form button.naturescot-forward-button').click();
+    cy.url().should('include', '/firearm');
+
+    cy.get('h2#error-summary-title').should('contain', 'There is a problem');
+
+    cy.get('.govuk-error-summary ul li a')
+      .should(
+        'contain',
+        'Your firearm certificate issue date can not be a future date.'
+        );
+  });
+
+  it('filled-out entries with past date + main button should navigate to same page with error', function () {
+    cy.visit('/firearm');
+
+    cy.get('input[type="text"]#certificate-number').type('123456789');
+    cy.get('input[type="text"]#certificate-issued-certificateIssuedDay').type('01');
+    cy.get('input[type="text"]#certificate-issued-certificateIssuedMonth').type('01');
+    cy.get('input[type="text"]#certificate-issued-certificateIssuedYear').type('1999');
+
+    cy.get('#main-content form button.naturescot-forward-button').click();
+    cy.url().should('include', '/firearm');
+
+    cy.get('h2#error-summary-title').should('contain', 'There is a problem');
+
+    cy.get('.govuk-error-summary ul li a')
+      .should(
+        'contain',
+        'Your firearm certificate must be less than five years old.'
+        );
   });
 });
