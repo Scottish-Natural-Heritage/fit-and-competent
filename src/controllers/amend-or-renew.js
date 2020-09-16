29b318d184a252a53d5e6f9c2938aa28a6c74f57
John Moore <john.moore@snh.gov.uk>
2020-09-16 11:26:42 +0100

    Add amend or renew controller
    Issue: Scottish-Natural-Heritage/Licensing#269

---
+++ src/controllers/amend-or-renew.js
@@ -0,0 +1,22 @@
+import {ReturnState} from './_base.js';
+
+const amendOrRenewController = (request) => {
+  // Did the user tell us they want do a new application.
+  if (request.body.applicationType === 'new') {
+    // Then we don't have any errors. This clears any previous errors.
+    request.session.applicationTypeError = false;
+    // Save the decision.
+    request.session.applicationType = false;
+    // Follow the 'new path'.
+    return ReturnState.Positive;
+  }
+
+  // The user submitted the form without selecting an option, this is an error!
+  request.session.applicationTypeError = true;
+  // Unset any saved value.
+  request.session.applicationType = undefined;
+  // Reload the page to highlight errors.
+  return ReturnState.Error;
+};
+
+export {amendOrRenewController as default};
