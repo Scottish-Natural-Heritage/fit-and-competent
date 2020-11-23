52a80be3881f1199f4f749448dafc2f4d091ab4c
John Moore <john.moore@snh.gov.uk>
2020-11-23 14:36:25 +0000

    Add env config
    Issue: Scottish-Natural-Heritage/Licensing#351

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@
 const config = Object.freeze({
   port: process.env.FC_PORT || '3008',
   sessionSecret: process.env.FC_SESSION_SECRET || 'override_this_value',
+  apiEndpoint: process.env.FC_API_URL ||'http://localhost:3009/fit-and-competent-api/v1',
   hostPrefix: process.env.FC_HOST_PREFIX || `http://localhost:${process.env.FC_PORT}`,
   pathPrefix: process.env.FC_PATH_PREFIX ? `/${process.env.FC_PATH_PREFIX}` : '/fit-and-competent',
   cookiePrefix: process.env.COOKIE_PREFIX || '__Secure'
